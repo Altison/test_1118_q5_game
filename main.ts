cff33a262764135d05503c1cdfd374a19c1b425e
Altison <71751499+Altison@users.noreply.github.com>
2020-11-18 15:55:15 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    sprite.move(1)
+})
 input.onButtonPressed(Button.AB, function () {
     if (sprite.get(LedSpriteProperty.Direction) == 0) {
         sprite.turn(Direction.Right, 90)
@@ -10,15 +13,13 @@ input.onButtonPressed(Button.B, function () {
     sprite.move(1)
     sprite.turn(Direction.Right, 180)
 })
-let list: number[] = []
 let sprite: game.LedSprite = null
 sprite = game.createSprite(0, 0)
+let x = randint(1, 4)
+let y = randint(1, 4)
 basic.forever(function () {
     sprite.ifOnEdgeBounce()
-    list = [sprite.get(LedSpriteProperty.X), sprite.get(LedSpriteProperty.Y)]
-    radio.sendString("" + (list))
-    if (sprite.get(LedSpriteProperty.X) == 4 && sprite.get(LedSpriteProperty.Y) == 4) {
-        basic.showNumber(0)
+    if (sprite.get(LedSpriteProperty.X) == x && sprite.get(LedSpriteProperty.Y) == y) {
         game.gameOver()
     }
 })
